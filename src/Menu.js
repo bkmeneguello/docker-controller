44a638348d73bbf27cda6ccd00fd0df842623c7c
Bruno Kühnen Meneguello <bruno@meneguello.com>
2016-12-06 23:23:54 -0200

    Render swarm only when swarm exists

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router'
+import { connect } from 'react-redux'
+import { Link, withRouter } from 'react-router';
+import Docker from './Docker';
 
 const ACTIVE_STYLE = {fontWeight: 'bold'};
 
-let Menu = withRouter(React.createClass({
+let Menu = withRouter(connect(
+  (state, ownProps) => {
+    return {
+      docker: new Docker(state.getIn(['hosts', ownProps.params.host]))
+    }
+  }
+)(React.createClass({
+  getInitialState: function() {
+    return {
+      swarm: false
+    };
+  },
   render: function() {
     let host = this.props.params.host;
     return (
@@ -12,22 +25,28 @@ let Menu = withRouter(React.createClass({
           <li className="sidebar-brand"><Link to="/">dashboard</Link></li>
           <li>
             <Link to="/hosts">hosts</Link>
-            {host &&
-            <ul>
-              <li><Link to={'/hosts/' + host + '/containers'} activeStyle={ACTIVE_STYLE}>containers</Link></li>
-              <li><Link to={'/hosts/' + host + '/images'} activeStyle={ACTIVE_STYLE}>images</Link></li>
-              <li><Link to={'/hosts/' + host + '/volumes'} activeStyle={ACTIVE_STYLE}>volumes</Link></li>
-              <li><Link to={'/hosts/' + host + '/networks'} activeStyle={ACTIVE_STYLE}>networks</Link></li>
-              <li><Link to={'/hosts/' + host + '/swarm'} activeStyle={ACTIVE_STYLE}>swarm</Link></li>
-              <li><Link to={'/hosts/' + host + '/swarm/services'} activeStyle={ACTIVE_STYLE}>services</Link></li>
-              <li><Link to={'/hosts/' + host + '/swarm/tasks'} activeStyle={ACTIVE_STYLE}>tasks</Link></li>
-            </ul>
-            }
+            {host && [
+              <Link to={'/hosts/' + host}>{host}</Link>,
+              <ul>
+                <li><Link to={'/hosts/' + host + '/containers'} activeStyle={ACTIVE_STYLE}>containers</Link></li>
+                <li><Link to={'/hosts/' + host + '/images'} activeStyle={ACTIVE_STYLE}>images</Link></li>
+                <li><Link to={'/hosts/' + host + '/volumes'} activeStyle={ACTIVE_STYLE}>volumes</Link></li>
+                <li><Link to={'/hosts/' + host + '/networks'} activeStyle={ACTIVE_STYLE}>networks</Link></li>
+                {this.state.swarm && [
+                <li><Link to={'/hosts/' + host + '/swarm'} activeStyle={ACTIVE_STYLE}>swarm</Link></li>,
+                <li><Link to={'/hosts/' + host + '/swarm/services'} activeStyle={ACTIVE_STYLE}>services</Link></li>,
+                <li><Link to={'/hosts/' + host + '/swarm/tasks'} activeStyle={ACTIVE_STYLE}>tasks</Link></li>
+                ]}
+              </ul>
+            ]}
           </li>
         </ul>
       </div>
     );
+  },
+  componentDidMount: function() {
+    this.props.docker.loadSwarm().then(() => this.setState({swarm: true}));
   }
-}));
+})));
 
 export default Menu;
